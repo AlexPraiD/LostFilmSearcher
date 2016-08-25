d4f06ed7db24b575029a596e205cc047dcae4f3f
AlexPraiD <PraiD-@mail.ru>
2016-08-25 17:24:45 +0300

    Добавил проект

---
+++ Unit2.h
@@ -0,0 +1,30 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit2H
+#define Unit2H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Data.DB.hpp>
+#include <Data.Win.ADODB.hpp>
+#include <Vcl.DBGrids.hpp>
+#include <Vcl.Grids.hpp>
+//---------------------------------------------------------------------------
+class TForm2 : public TForm
+{
+__published:	// IDE-managed Components
+	TDataSource *DataSource1;
+	TADOQuery *ADOQuery1;
+	TDBGrid *DBGrid1;
+	TButton *Button4;
+	void __fastcall Button4Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm2(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm2 *Form2;
+//---------------------------------------------------------------------------
+#endif
